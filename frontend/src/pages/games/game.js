5b58a43b68ae8e0c3578c8b9391336e8b2af2f41
tiziano <tizianoharo09@gmail.com>
2025-11-18 22:01:34 -0300

    feat: reestructuracion de la "notification" de producto añadido al carrito
    - "notificacion" pasa a ser "toast"
    - implementar animaciones para una mejor UX

--- frontend/src/pages/games/game.js
+++ frontend/src/pages/games/game.js
@@ -1,9 +1,9 @@
 const gameContainer = document.querySelector(".game-presentation");
 const addToCartBtn = document.getElementById("add-to-cart");
-const notificationContainer = document.getElementById("notification");
-const notificationTitle = document.getElementById("notification-title");
-const notificationIcon = document.getElementById("notification-icon");
-const notificationContent = document.getElementById("notification-content");
+const toastContainer = document.getElementById("toast");
+const toastTitle = document.getElementById("toast-title");
+const toastIcon = document.getElementById("toast-icon");
+const toastContent = document.getElementById("toast-content");
 
 const getGameData = (container) => {
   const tags = container.querySelectorAll(".game-tag");
@@ -29,32 +29,33 @@ const saveToCart = (game) => {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-const showNotification = (status, title, content) => {
-  (status === "error")
-    ? notificationIcon.src = "/src/assets/error-circle.svg"
-    : notificationIcon.src = "/src/assets/check-circle.svg";
-
-  notificationContainer.classList.add(status);
-  notificationTitle.textContent = title;
-  notificationContent.textContent = content;
+const showToast = (status, title, content) => {
+  status === "error"
+  ? toastIcon.src = "/src/assets/error-circle.svg"
+  : toastIcon.src = "/src/assets/check-circle.svg";
+  
+  toastContainer.classList.add(status);
+  toastContainer.classList.add("show");
+  toastTitle.textContent = title;
+  toastContent.textContent = content;
   
   setTimeout(() => {
     addToCartBtn.removeAttribute("disabled");
-    notificationContainer.classList.remove("success", "error");
-    notificationContainer.classList.add("hidden");
+    toastContainer.classList.remove("success", "error");
+    toastContainer.classList.remove("show");
   }, 3000);
 }
 
 addToCartBtn.addEventListener("click", () => {
   const isLogged = localStorage.getItem("token");
-  notificationContainer.classList.remove("hidden");
+  toastContainer.classList.remove("hidden");
   addToCartBtn.setAttribute("disabled", true);
 
   if (!isLogged) {
-    return showNotification("error", "Error", "Inicie sesion para administrar su carrito");
+    return showToast("error", "Error", "Inicie sesion para administrar su carrito");
   }
   
-  showNotification("success", "Exito", "Se ha añadido el producto correctamente al carrito");
+  showToast("success", "Exito", "Se ha añadido el producto correctamente al carrito");
   
   const gameData = getGameData(gameContainer);
   saveToCart(gameData);
